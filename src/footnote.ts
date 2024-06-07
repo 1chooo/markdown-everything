60bf2178ffabe9e0037ff6931ba4f852180732eb
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-07 23:38:25 +0800

    style: extendMarkdownIt

--- src/footnote.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-export function renderFootnote(md: any) {
-	if (vscode.window.activeTextEditor?.document.languageId === 'markdown') {
-		return md.use(require('markdown-it-footnote'));
-	}
-	return md;
-}
